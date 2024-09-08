9c6c9b14325c48d5a69a170a9daeff5bfd274145
Chris Moore <chrismoorelmt@gmail.com>
2024-09-07 22:01:13 -0500

    Ready to deploy

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,6 @@ const withAuth = require('../util/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
     const blogData = await Blog.findAll({
       include: [
         {
@@ -14,10 +13,8 @@ router.get('/', async (req, res) => {
       ],
     });
 
-    // Serialize data so the template can read it
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
     res.render('homepage', { 
       blogs, 
       logged_in: req.session.logged_in 
@@ -27,7 +24,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/blog/:id', async (req, res) => { //Post ID => filters username
+router.get('/blog/:id', async (req, res) => {
   try {
     const blogData = await Blog.findByPk(req.params.id, {
       include: [
@@ -49,10 +46,8 @@ router.get('/blog/:id', async (req, res) => { //Post ID => filters username
   }
 });
 
-// Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
   try {
-    // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
       include: [{ model: Blog }],
@@ -70,7 +65,6 @@ router.get('/profile', withAuth, async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect('/profile');
     return;
--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
 DROP DATABASE IF EXISTS blogsite_db;
 CREATE DATABASE blogsite_db;
 
-\c blogspace_db;
+\c blogsite_db;
